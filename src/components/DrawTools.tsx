cb9cfce27085671b0c9f03a17100ce07e7eb7f2f
hnm <hnm@hnmui-Macmini.local>
2025-07-03 17:56:34 +0900

    Add Drawing Circle, Rect function

--- src/components/DrawTools.tsx
+++ src/components/DrawTools.tsx
@@ -1,7 +1,7 @@
 import { Feature, FeatureCollection, LineString, Polygon } from "geojson";
 import "maplibre-gl/dist/maplibre-gl.css";
 import { useCallback, useEffect, useRef, useState } from "react";
-import { Layer, MapRef, Source } from "react-map-gl/maplibre";
+import { Layer, MapRef, Marker, Source } from "react-map-gl/maplibre";
 import { styled } from "styled-components";
 
 interface DrawToolsProps {
@@ -19,57 +19,189 @@ const IconDiv = styled.div<{ padding: number }>`
   padding: ${(props) => props.padding}px;
   box-sizing: border-box;
 `;
+const POLY = "polygon";
+const CIRC = "circle";
+const RECT = "rect";
+type DrawMode = "polygon" | "circle" | "rect" | null;
+
+const RadiusLabel = styled.div`
+  background: rgba(255, 255, 255, 0.95);
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  padding: 4px 10px;
+  font-weight: 600;
+  font-size: 15px;
+  color: #333;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.08);
+  pointer-events: none;
+  transform: translateY(-10px);
+  white-space: nowrap;
+`;
 
 function DrawTools({ mapRef }: DrawToolsProps) {
   const [isDrawing, setIsDrawing] = useState(false);
+  const [drawMode, setDrawMode] = useState<DrawMode>(null);
   const [polygons, setPolygons] = useState<Array<Array<[number, number]>>>([]);
+  const [circles, setCircles] = useState<
+    Array<{ center: [number, number]; radius: number }>
+  >([]);
   const [points, setPoints] = useState<[number, number][]>([]);
   const [cursor, setCursor] = useState<{ lng: number; lat: number } | null>(
     null
   );
   const [isToolClicked, setIsToolClicked] = useState<boolean>();
+  const [circleTemp, setCircleTemp] = useState<{
+    center: [number, number];
+    radius: number;
+  } | null>(null);
+  const [rectangles, setRectangles] = useState<
+    Array<[[number, number], [number, number]]>
+  >([]);
+  const [rectTemp, setRectTemp] = useState<{
+    start: [number, number];
+    end: [number, number];
+  } | null>(null);
 
-  // 최신 points를 항상 참조하기 위한 ref
   const pointsRef = useRef(points);
   useEffect(() => {
     pointsRef.current = points;
   }, [points]);
 
-  // 이벤트 등록
+  useEffect(() => {
+    console.log("##rectTemp", rectTemp);
+  }, [rectTemp]);
+
   useEffect(() => {
     if (!mapRef.current) return;
     const map = mapRef.current.getMap();
 
     if (!isDrawing) return;
+    if (drawMode === POLY) {
+      const handleClick = (e: maplibregl.MapMouseEvent) => {
+        const { lng, lat } = e.lngLat;
+        setPoints((prev) => [...prev, [lng, lat]]);
+      };
+      const handleDblClick = (e: maplibregl.MapMouseEvent) => {
+        e.preventDefault();
+        finishDrawing();
+      };
+      const handleMouseMove = (e: maplibregl.MapMouseEvent) => {
+        if (pointsRef.current.length >= 2) {
+          setCursor({ lng: e.lngLat.lng, lat: e.lngLat.lat });
+        }
+      };
+      map.on("click", handleClick);
+      map.on("dblclick", handleDblClick);
+      map.on("mousemove", handleMouseMove);
+      return () => {
+        map.off("click", handleClick);
+        map.off("dblclick", handleDblClick);
+        map.off("mousemove", handleMouseMove);
+      };
+    }
+    if (drawMode === CIRC) {
+      let wheelTimeout: NodeJS.Timeout | null = null;
+      const handleClick = (e: maplibregl.MapMouseEvent) => {
+        if (!circleTemp) {
+          setCircleTemp({ center: [e.lngLat.lng, e.lngLat.lat], radius: 100 });
+        } else {
+          setCircles((prev) => [
+            ...prev,
+            { center: circleTemp.center, radius: circleTemp.radius },
+          ]);
+          setCircleTemp(null);
+          setIsDrawing(false);
+          setDrawMode(null);
+        }
+      };
+      const handleWheel = (e: maplibregl.MapWheelEvent) => {
+        if (circleTemp) {
+          e.preventDefault();
+          let nextRadius =
+            circleTemp.radius + (e.originalEvent.deltaY > 0 ? 20 : -20);
+          if (nextRadius < 20) nextRadius = 20;
+          setCircleTemp({ ...circleTemp, radius: nextRadius });
+        }
+      };
+      map.on("click", handleClick);
+      map.on("wheel", handleWheel);
+      return () => {
+        map.off("click", handleClick);
+        map.off("wheel", handleWheel);
+      };
+    }
+    if (drawMode === RECT) {
+      let isRectStarted = false;
+      const handleClick = (e: maplibregl.MapMouseEvent) => {
+        console.log("##??", isRectStarted, rectTemp);
+        if (!rectTemp) {
+          setRectTemp({
+            start: [e.lngLat.lng, e.lngLat.lat],
+            end: [e.lngLat.lng, e.lngLat.lat],
+          });
+          isRectStarted = true;
+        }
+      };
+      const handleMouseMove = (e: maplibregl.MapMouseEvent) => {
+        if (rectTemp) {
+          setRectTemp({ ...rectTemp, end: [e.lngLat.lng, e.lngLat.lat] });
+        }
+      };
+      const handleDblClick = (e: maplibregl.MapMouseEvent) => {
+        if (rectTemp) {
+          e.preventDefault();
+          setRectTemp({ ...rectTemp, end: [e.lngLat.lng, e.lngLat.lat] });
+          setRectangles((prev) => [
+            ...prev,
+            [rectTemp.start, [e.lngLat.lng, e.lngLat.lat]],
+          ]);
+          setRectTemp(null);
+          setIsDrawing(false);
+          setDrawMode(null);
+        }
+      };
+      map.on("click", handleClick);
+      map.on("mousemove", handleMouseMove);
+      map.on("dblclick", handleDblClick);
+      return () => {
+        map.off("click", handleClick);
+        map.off("mousemove", handleMouseMove);
+        map.off("dblclick", handleDblClick);
+      };
+    }
+  }, [mapRef, isDrawing, drawMode, circleTemp, rectTemp]);
 
-    const handleClick = (e: maplibregl.MapMouseEvent) => {
-      const { lng, lat } = e.lngLat;
-      setPoints((prev) => [...prev, [lng, lat]]);
-    };
+  function getDistance(a: [number, number], b: [number, number]) {
+    const R = 6371000;
+    const toRad = (d: number) => (d * Math.PI) / 180;
+    const dLat = toRad(b[1] - a[1]);
+    const dLng = toRad(b[0] - a[0]);
+    const lat1 = toRad(a[1]);
+    const lat2 = toRad(b[1]);
+    const aVal =
+      Math.sin(dLat / 2) ** 2 +
+      Math.cos(lat1) * Math.cos(lat2) * Math.sin(dLng / 2) ** 2;
+    const c = 2 * Math.atan2(Math.sqrt(aVal), Math.sqrt(1 - aVal));
+    return R * c;
+  }
 
-    const handleDblClick = (e: maplibregl.MapMouseEvent) => {
-      e.preventDefault();
-      finishDrawing();
-    };
+  function circleToPolygon(
+    center: [number, number],
+    radius: number,
+    steps = 64
+  ): [number, number][] {
+    const coords: [number, number][] = [];
+    for (let i = 0; i <= steps; i++) {
+      const angle = (i / steps) * 2 * Math.PI;
+      const dx = radius * Math.cos(angle);
+      const dy = radius * Math.sin(angle);
+      const deltaLng = dx / (111320 * Math.cos((center[1] * Math.PI) / 180));
+      const deltaLat = dy / 110540;
+      coords.push([center[0] + deltaLng, center[1] + deltaLat]);
+    }
+    return coords;
+  }
 
-    const handleMouseMove = (e: maplibregl.MapMouseEvent) => {
-      if (pointsRef.current.length >= 2) {
-        setCursor({ lng: e.lngLat.lng, lat: e.lngLat.lat });
-      }
-    };
-
-    map.on("click", handleClick);
-    map.on("dblclick", handleDblClick);
-    map.on("mousemove", handleMouseMove);
-
-    return () => {
-      map.off("click", handleClick);
-      map.off("dblclick", handleDblClick);
-      map.off("mousemove", handleMouseMove);
-    };
-  }, [mapRef, isDrawing]);
-
-  // 그리기 종료
   const finishDrawing = useCallback(() => {
     if (pointsRef.current.length < 3) {
       alert("폴리곤은 최소 3개의 점이 필요해요!");
@@ -83,11 +215,7 @@ function DrawTools({ mapRef }: DrawToolsProps) {
 
   const polygonCoords =
     points.length >= 2 && cursor
-      ? [
-          ...points,
-          [cursor.lng, cursor.lat],
-          points[0], // 폴리곤 닫기
-        ]
+      ? [...points, [cursor.lng, cursor.lat], points[0]]
       : null;
 
   const polygonGeoJSON: FeatureCollection<Polygon> = {
@@ -97,7 +225,7 @@ function DrawTools({ mapRef }: DrawToolsProps) {
       properties: { id: i },
       geometry: {
         type: "Polygon",
-        coordinates: [[...coords, coords[0]]], // 첫점 닫기
+        coordinates: [[...coords, coords[0]]],
       },
     })),
   };
@@ -126,6 +254,79 @@ function DrawTools({ mapRef }: DrawToolsProps) {
     })),
   };
 
+  const circlesGeoJSON: FeatureCollection<Polygon> = {
+    type: "FeatureCollection",
+    features: circles.map((circle, i) => ({
+      type: "Feature",
+      properties: { id: i },
+      geometry: {
+        type: "Polygon",
+        coordinates: [circleToPolygon(circle.center, circle.radius)],
+      },
+    })),
+  };
+
+  const tempCircleGeoJSON: FeatureCollection<Polygon> | null = circleTemp
+    ? {
+        type: "FeatureCollection",
+        features: [
+          {
+            type: "Feature",
+            properties: {},
+            geometry: {
+              type: "Polygon",
+              coordinates: [
+                circleToPolygon(circleTemp.center, circleTemp.radius),
+              ],
+            },
+          },
+        ],
+      }
+    : null;
+
+  function rectToPolygon(
+    start: [number, number],
+    end: [number, number]
+  ): [number, number][] {
+    const [x1, y1] = start;
+    const [x2, y2] = end;
+    return [
+      [x1, y1],
+      [x2, y1],
+      [x2, y2],
+      [x1, y2],
+      [x1, y1],
+    ];
+  }
+
+  const rectanglesGeoJSON: FeatureCollection<Polygon> = {
+    type: "FeatureCollection",
+    features: rectangles.map((rect, i) => ({
+      type: "Feature",
+      properties: { id: i },
+      geometry: {
+        type: "Polygon",
+        coordinates: [rectToPolygon(rect[0], rect[1])],
+      },
+    })),
+  };
+
+  const tempRectGeoJSON: FeatureCollection<Polygon> | null = rectTemp
+    ? {
+        type: "FeatureCollection",
+        features: [
+          {
+            type: "Feature",
+            properties: {},
+            geometry: {
+              type: "Polygon",
+              coordinates: [rectToPolygon(rectTemp.start, rectTemp.end)],
+            },
+          },
+        ],
+      }
+    : null;
+
   return (
     <div>
       <div className="maplibregl-ctrl-top-right">
@@ -153,8 +354,13 @@ function DrawTools({ mapRef }: DrawToolsProps) {
                 style={{
                   borderRight: "1px solid #ddd",
                   borderTop: "none",
+                  opacity: isDrawing && drawMode !== POLY ? "0.5" : "1",
                 }}
-                onClick={() => setIsDrawing(true)}
+                onClick={() => {
+                  setIsDrawing(true);
+                  setDrawMode(POLY);
+                }}
+                disabled={isDrawing && drawMode !== POLY}
               >
                 <IconDiv padding={0}>
                   <img src="/polygon.png" alt="draw" height={20} />
@@ -169,7 +375,13 @@ function DrawTools({ mapRef }: DrawToolsProps) {
                   borderRight: "1px solid #ddd",
                   borderLeft: "1px solid #ddd",
                   borderTop: "none",
+                  opacity: isDrawing && drawMode !== RECT ? "0.5" : "1",
                 }}
+                onClick={() => {
+                  setIsDrawing(true);
+                  setDrawMode(RECT);
+                }}
+                disabled={isDrawing && drawMode !== RECT}
               >
                 <IconDiv padding={2}>
                   <img src="/rectangle.png" alt="draw" height={20} />
@@ -183,7 +395,13 @@ function DrawTools({ mapRef }: DrawToolsProps) {
                 style={{
                   borderLeft: "1px solid #ddd",
                   borderTop: "none",
+                  opacity: isDrawing && drawMode !== CIRC ? "0.5" : "1",
                 }}
+                onClick={() => {
+                  setIsDrawing(true);
+                  setDrawMode(CIRC);
+                }}
+                disabled={isDrawing && drawMode !== CIRC}
               >
                 <IconDiv padding={2}>
                   <img src="/circle.png" alt="draw" height={20} />
@@ -193,7 +411,7 @@ function DrawTools({ mapRef }: DrawToolsProps) {
           )}
         </div>
       </div>
-
+      {/* 완성 폴리곤 */}
       <Source id="polygons" type="geojson" data={polygonGeoJSON}>
         <Layer
           id="polygon-fill"
@@ -206,8 +424,34 @@ function DrawTools({ mapRef }: DrawToolsProps) {
           paint={{ "line-color": "orange", "line-width": 2 }}
         />
       </Source>
-
-      {polygonCoords && (
+      {/* 완성 원 */}
+      <Source id="circles" type="geojson" data={circlesGeoJSON}>
+        <Layer
+          id="circle-fill"
+          type="fill"
+          paint={{ "fill-color": "rgba(248, 161, 55, 0.41)" }}
+        />
+        <Layer
+          id="circle-line"
+          type="line"
+          paint={{ "line-color": "orange", "line-width": 2 }}
+        />
+      </Source>
+      {/* 완성 사각형 */}
+      <Source id="rectangles" type="geojson" data={rectanglesGeoJSON}>
+        <Layer
+          id="rectangle-fill"
+          type="fill"
+          paint={{ "fill-color": "rgba(248, 161, 55, 0.41)" }}
+        />
+        <Layer
+          id="rectangle-line"
+          type="line"
+          paint={{ "line-color": "orange", "line-width": 2 }}
+        />
+      </Source>
+      {/* 폴리곤 임시 모양 */}
+      {drawMode === POLY && polygonCoords && (
         <Source
           id="temp-polygon"
           type="geojson"
@@ -224,36 +468,83 @@ function DrawTools({ mapRef }: DrawToolsProps) {
           <Layer
             id="temp-polygon-layer"
             type="fill"
-            paint={{ "fill-color": "#088", "fill-opacity": 0.5 }}
+            paint={{ "fill-color": "#088", "fill-opacity": 0.3 }}
           />
         </Source>
       )}
-
-      {currentLineString && (
+      {/* 폴리곤 선 */}
+      {drawMode === POLY && currentLineString && (
+        <Source id="current-line" type="geojson" data={currentLineString}>
+          <Layer
+            id="current-line-layer"
+            type="line"
+            paint={{
+              "line-color": "blue",
+              "line-width": 2,
+              "line-dasharray": [2, 2],
+            }}
+          />
+        </Source>
+      )}
+      {/* 폴리곤 점 */}
+      {drawMode === POLY && (
+        <Source id="current-points" type="geojson" data={pointsGeoJSON}>
+          <Layer
+            id="current-points-layer"
+            type="circle"
+            paint={{
+              "circle-color": "blue",
+              "circle-radius": 5,
+            }}
+          />
+        </Source>
+      )}
+      {/* 임시 원 */}
+      {tempCircleGeoJSON && (
         <>
-          <Source id="current-line" type="geojson" data={currentLineString}>
+          <Source id="temp-circle" type="geojson" data={tempCircleGeoJSON}>
             <Layer
-              id="current-line-layer"
-              type="line"
+              id="temp-circle-fill"
+              type="fill"
               paint={{
-                "line-color": "blue",
-                "line-width": 2,
-                "line-dasharray": [2, 2],
+                "fill-color": "#088",
+                "fill-opacity": 0.3,
+                "fill-outline-color": "blue",
               }}
             />
           </Source>
+          {circleTemp && (
+            <Marker
+              longitude={circleTemp.center[0]}
+              latitude={circleTemp.center[1]}
+              anchor="bottom"
+            >
+              <RadiusLabel>
+                반지름: {Math.round(circleTemp.radius)} m
+              </RadiusLabel>
+            </Marker>
+          )}
         </>
       )}
-      <Source id="current-points" type="geojson" data={pointsGeoJSON}>
-        <Layer
-          id="current-points-layer"
-          type="circle"
-          paint={{
-            "circle-color": "blue",
-            "circle-radius": 5,
-          }}
-        />
-      </Source>
+      {/* 임시 사각형 */}
+      {tempRectGeoJSON && (
+        <Source id="temp-rectangle" type="geojson" data={tempRectGeoJSON}>
+          <Layer
+            id="temp-rectangle-fill"
+            type="fill"
+            paint={{ "fill-color": "#088", "fill-opacity": 0.3 }}
+          />
+          <Layer
+            id="temp-rectangle-line"
+            type="line"
+            paint={{
+              "line-color": "blue",
+              "line-width": 2,
+              "line-dasharray": [2, 2],
+            }}
+          />
+        </Source>
+      )}
     </div>
   );
 }
